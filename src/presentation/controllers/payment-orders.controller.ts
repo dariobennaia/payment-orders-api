fa4f82c5e87dce2b5a1aed03c56cc6d5f390e433
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-07 21:01:07 -0300

    refactor: add protocols

--- src/presentation/controllers/payment-orders.controller.ts
+++ src/presentation/controllers/payment-orders.controller.ts
@@ -1,6 +1,18 @@
-export class PaymentOrdersController {
-  handle(httpRequest: any): any {
+import { Controller, HttpResponse } from '@/presentation/protocols';
+
+export class PaymentOrdersController implements Controller {
+  async handle(
+    httpRequest: PaymentOrdersController.Request,
+  ): Promise<HttpResponse> {
     const body = { internalId: 'any_id', status: 'any_status' };
     return { statusCode: 201, body };
   }
+}
+
+export namespace PaymentOrdersController {
+  export type Request = {
+    externalId: string;
+    mount: number;
+    expectedOn: Date;
+  };
 }
