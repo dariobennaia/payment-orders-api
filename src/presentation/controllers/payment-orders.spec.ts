233bd85e9c87f469e1da02eb785b6571f09878af
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-07 21:02:23 -0300

    refactor: ensure it should returns a promise data

--- src/presentation/controllers/payment-orders.spec.ts
+++ src/presentation/controllers/payment-orders.spec.ts
@@ -1,7 +1,7 @@
 import { PaymentOrdersController } from '@/presentation/controllers/payment-orders.controller';
 
 describe('Payment Orders Controller', () => {
-  test('Should return transaction created', () => {
+  test('Should return transaction created', async () => {
     const sut = new PaymentOrdersController();
 
     const httpRequest = {
@@ -12,8 +12,11 @@ describe('Payment Orders Controller', () => {
       },
     };
 
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest.body);
     expect(httpResponse.statusCode).toBe(201);
-    expect(httpResponse.body).toEqual({ internalId: 'any_id', status: 'any_status' });
+    expect(httpResponse.body).toEqual({
+      internalId: 'any_id',
+      status: 'any_status',
+    });
   });
 });
