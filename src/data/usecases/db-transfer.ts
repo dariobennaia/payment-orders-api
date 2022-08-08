ca1d22deab5d76f18a3a8edbd51790910e985de3
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-08 19:36:08 -0300

    feat: ensure return created transfer after send bank api

--- src/data/usecases/db-transfer.ts
+++ src/data/usecases/db-transfer.ts
@@ -1,7 +1,16 @@
+import { BankApi } from '@/data/protocols';
 import { Transfer } from '@/domain/usecases/transfer';
 
 export class DbTransfer implements Transfer {
-  async send(params: Transfer.Params): Promise<Transfer.Result> {
+  constructor(private readonly bankTransferApi: BankApi) {}
+
+  async send(params: DbTransfer.Params): Promise<DbTransfer.Result> {
+    await this.bankTransferApi.send(params);
     return { internalId: '', status: 'CREATED' };
   }
+}
+
+export namespace DbTransfer {
+  export type Params = Transfer.Params;
+  export type Result = Transfer.Result;
 }
