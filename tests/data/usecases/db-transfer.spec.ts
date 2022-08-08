a40bbe192e1899dcdda55fe43f13583c42a17a8d
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-08 19:36:30 -0300

    test: ensure return created transfer after send bank api

--- tests/data/usecases/db-transfer.spec.ts
+++ tests/data/usecases/db-transfer.spec.ts
@@ -1,15 +1,19 @@
 import { DbTransfer } from '@/data/usecases';
 import { Transfer } from '@/domain/usecases/transfer';
+import { BankApiService } from '@/infra/bank';
 import { mockRequest } from '@/tests/presentation/mocks';
 
 type SutType = {
   sut: Transfer;
+  bankApiServiceMockSpy: BankApiService;
 };
 
 const makeSut = (): SutType => {
-  const sut = new DbTransfer();
+  const bankApiServiceMockSpy = new BankApiService();
+  const sut = new DbTransfer(bankApiServiceMockSpy);
   return {
     sut,
+    bankApiServiceMockSpy,
   };
 };
 
@@ -18,6 +22,19 @@ describe('Db Transfer', () => {
     const { sut } = makeSut();
     const { body } = mockRequest();
     const created = await sut.send(body);
+    expect(created).toEqual({ internalId: '', status: 'CREATED' });
+  });
+
+  test('Should schedule transfer with bank api', async () => {
+    const { sut, bankApiServiceMockSpy } = makeSut();
+
+    const { body } = mockRequest();
+    const created = await sut.send(body);
+
+    jest
+      .spyOn(bankApiServiceMockSpy, 'send')
+      .mockImplementationOnce(async () => null);
+
     expect(created).toEqual({ internalId: '', status: 'CREATED' });
   });
 });
