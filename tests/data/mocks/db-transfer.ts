d1686b33f3c305f625fef95994c98362b7d03a47
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-08 23:02:15 -0300

    refactor: add mock repository result

--- tests/data/mocks/db-transfer.ts
+++ tests/data/mocks/db-transfer.ts
@@ -1,5 +1,18 @@
-import { TransferApi } from '@/data/protocols';
+import { TransferApi, TransferRepository } from '@/data/protocols';
+import { TransferModel } from '@/domain/models';
 import { Transfer } from '@/domain/usecases';
+import { faker } from '@faker-js/faker';
+
+export const resultTransferRepository = (
+  params: Partial<TransferModel>,
+): TransferModel => ({
+  id: faker.database.mongodbObjectId(),
+  status: 'CREATED',
+  amount: Number(faker.commerce.price()),
+  expectedOn: new Date(),
+  externalId: faker.datatype.uuid(),
+  ...params,
+});
 
 export class TransferApiMock implements TransferApi {
   public params: Transfer.Params;
@@ -7,5 +20,19 @@ export class TransferApiMock implements TransferApi {
   async send(params: Transfer.Params): Promise<any> {
     this.params = params;
     return {};
+  }
+}
+export class TransferMongoRepositoryMock implements TransferRepository {
+  params: TransferRepository.Params;
+
+  result: TransferModel;
+
+  async save(params: TransferRepository.Params): Promise<TransferModel> {
+    this.params = params;
+    return {
+      id: faker.database.mongodbObjectId(),
+      status: 'CREATED',
+      ...params,
+    };
   }
 }
