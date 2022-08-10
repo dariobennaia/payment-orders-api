f7586914c59a51caadca9e15ec3beaf19bccb89c
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 19:21:54 -0300

    test: should return an error if object field

--- tests/validations/is-string-field-validation.spec.ts
+++ tests/validations/is-string-field-validation.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidTypeParamError } from '@/presentation/errors';
 import { Validation } from '@/presentation/protocols';
 import { IsStringFieldValidation } from '@/validations';
 
@@ -19,5 +20,11 @@ describe('Is String Validation', () => {
     const validate = sut.validate({ anyField });
     expect(typeof anyField).toBe('string');
     expect(validate).toBe(undefined);
+  });
+
+  test('Should return an error if object field', async () => {
+    const { sut } = makeSut();
+    const validate = sut.validate({ anyField: {} });
+    expect(validate).toEqual(new InvalidTypeParamError('anyField'));
   });
 });
