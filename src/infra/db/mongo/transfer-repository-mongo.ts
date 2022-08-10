644f34c2c4cad9d8000d9041bf6ebe5feef29cdf
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 11:48:50 -0300

    refactor: add sanitize id mongo helper

--- src/infra/db/mongo/transfer-repository-mongo.ts
+++ src/infra/db/mongo/transfer-repository-mongo.ts
@@ -20,8 +20,8 @@ implements
       ...params,
       status: [{ name: params.status.name, date: new Date() }],
     };
-    const transfer = await transferCollection.insertOne(data);
-    return { id: String(transfer.insertedId), ...params };
+    const { insertedId } = await transferCollection.insertOne(data);
+    return MongoHelper.sanitize<TransferModel>({ insertedId, ...params });
   }
 
   async updateById(
@@ -32,14 +32,9 @@ implements
     const transferCollection = MongoHelper.getCollection('transfer');
     params.status.date = new Date();
     await transferCollection.updateOne({ _id }, { $push: params });
-    const { _id: idFinded, ...rest } = await transferCollection.findOne({
-      _id,
-    });
-    rest.status = rest.status.reverse()[0];
-    return Object.assign(<UpdateTransferRepository.Result>{}, {
-      id: idFinded,
-      ...rest,
-    });
+    const finded = await transferCollection.findOne({ _id });
+    finded.status = finded.status.reverse()[0];
+    return MongoHelper.sanitize<UpdateTransferRepository.Result>(finded);
   }
 
   async findByParams({
