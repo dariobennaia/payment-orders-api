062fc117085f130dc2022a2e1003861ac26c2a2f
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 00:29:57 -0300

    feat: ensure update transfer

--- src/infra/db/mongo/transfer-repository-mongo.ts
+++ src/infra/db/mongo/transfer-repository-mongo.ts
@@ -1,8 +1,15 @@
-import { CreateTransferRepository } from '@/data/protocols';
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable prefer-destructuring */
+import {
+  CreateTransferRepository,
+  UpdateTransferRepository,
+} from '@/data/protocols';
 import { TransferModel } from '@/domain/models';
 import { MongoHelper } from '@/infra/db/mongo';
+import { ObjectId } from 'mongodb';
 
-export class TransferRepositoryMongo implements CreateTransferRepository {
+export class TransferRepositoryMongo
+implements CreateTransferRepository, UpdateTransferRepository {
   async save(params: CreateTransferRepository.Params): Promise<TransferModel> {
     const transferCollection = MongoHelper.getCollection('transfer');
     const data = {
@@ -11,5 +18,18 @@ export class TransferRepositoryMongo implements CreateTransferRepository {
     };
     const transfer = await transferCollection.insertOne(data);
     return { id: String(transfer.insertedId), ...params };
+  }
+
+  async updateById(
+    id: string,
+    params: UpdateTransferRepository.Params,
+  ): Promise<UpdateTransferRepository.Result> {
+    const _id = new ObjectId(id);
+    const transferCollection = MongoHelper.getCollection('transfer');
+    params.status.date = new Date();
+    await transferCollection.updateOne({ _id }, { $push: params });
+    const { _id: idFinded, ...rest } = await transferCollection.findOne({ _id });
+    rest.status = rest.status.reverse()[0];
+    return Object.assign(<UpdateTransferRepository.Result>{}, { id: idFinded, ...rest });
   }
 }
