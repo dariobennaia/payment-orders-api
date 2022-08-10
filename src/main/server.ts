3f377b56fb3b257b66735526b94437d9dfb60a72
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 17:31:43 -0300

    feat: add connection mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,10 @@
+/* eslint-disable no-console */
+import { MongoHelper } from '@/infra/db';
 import { main } from '@/main/config/app';
 import env from '@/main/config/env';
 
-main().listen(env.port);
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    main().listen(env.port);
+  })
+  .catch(console.error);
