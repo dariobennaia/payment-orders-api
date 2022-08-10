eff68218764c8ed1b89e7fe686a010d0abecae40
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-09 22:59:17 -0300

    test: ensure create transfer

---
+++ tests/infra/db/mongo/transfer-repository-mongo.spec.ts
@@ -0,0 +1,35 @@
+import { MongoHelper, TransferRepositoryMongo } from '@/infra/db';
+import { dataCreateTransferMock } from '@/tests/infra/mocks';
+
+type SutType = {
+  sut: TransferRepositoryMongo;
+};
+
+const makeSut = (): SutType => ({
+  sut: new TransferRepositoryMongo(),
+});
+
+describe('Transfer Repository Mongo', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.getCollection('transfer').drop().catch(() => null);
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await MongoHelper.getCollection('transfer').drop().catch(() => null);
+  });
+
+  describe('save()', () => {
+    test('Should return an account on success', async () => {
+      const { sut } = makeSut();
+      const addAccountParams = dataCreateTransferMock();
+      const { id, ...rest } = await sut.save(addAccountParams);
+      expect(rest).toEqual(addAccountParams);
+      expect(id).toBeDefined();
+    });
+  });
+});
