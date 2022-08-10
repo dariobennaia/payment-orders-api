5be04ccaf160aa6e754cced68f3853f858256ed8
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 17:31:17 -0300

    feat: integrate midlewares

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 
+import middlewares from './middlewares';
 import routes from './routes';
 
 export const main = (): Express => {
   const app = express();
+  middlewares(app);
   routes(app);
   return app;
 };
