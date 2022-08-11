264521d8c28d45aabc6fb162ead3a888ef1ab2fc
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-11 14:09:29 -0300

    feat: ensure process payment order

--- src/main/factories/jobs/make-process-payment-order.ts
+++ src/main/factories/jobs/make-process-payment-order.ts
@@ -1,10 +1,13 @@
 import { JobProcessPaymentOrder } from '@/data/usecases';
+import { TransferRepositoryMongo } from '@/infra/db';
 import { Schedule } from '@/infra/schedule';
 import { ProcessPaymentOrderJob } from '@/presentation/jobs';
 
 const makeProcess = () => {
   const schedule = new Schedule();
-  return new JobProcessPaymentOrder(schedule);
+  const findRepository = new TransferRepositoryMongo();
+  const updateRepository = new TransferRepositoryMongo();
+  return new JobProcessPaymentOrder(schedule, findRepository, updateRepository);
 };
 
 export const makeProcessPaymentOrder = (): void => {
