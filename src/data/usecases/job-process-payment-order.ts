011d9ce2dc02169b413dc3dfa3445dfb0fa5b5b6
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-11 14:22:18 -0300

    refactor: shufle new status payment order

--- src/data/usecases/job-process-payment-order.ts
+++ src/data/usecases/job-process-payment-order.ts
@@ -7,6 +7,7 @@ import {
 } from '@/data/protocols';
 import { ProcessPaymentOrder } from '@/domain/usecases';
 
+const shufleNewStatus = (): any => ['APPROVED', 'REJECTED'].sort(() => 0.5 - Math.random())[0];
 export class JobProcessPaymentOrder implements ProcessPaymentOrder {
   constructor(
     private readonly jobSchedule: SchedulePaymentOrder,
@@ -36,14 +37,14 @@ export class JobProcessPaymentOrder implements ProcessPaymentOrder {
     for (const { id, status, expectedOn } of paymentsOrders) {
       const scheduled = status.filter((f) => f.name === 'SCHEDULED').length > 0;
       if (scheduled && new Date(expectedOn) > new Date()) {
-        console.log('Ignore', id);
+        console.log(`Payment Order ${id} Scheduled`);
         continue;
       }
 
       this.updateRepository.updateById(id, {
-        status: { name: 'APPROVED' },
+        status: { name: shufleNewStatus() },
       });
-      console.log('Envie esse', id);
+      console.log(`Executing Payment Order ${id}`);
     }
   }
 
