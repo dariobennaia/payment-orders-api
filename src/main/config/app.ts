781341b90434811f125d74bf70bcb3eef4ce21ed
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-11 18:38:48 -0300

    feat: add config swagger server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,10 +2,12 @@ import express, { Express } from 'express';
 
 import middlewares from './middlewares';
 import routes from './routes';
+import swagger from './swagger';
 
 export const main = (): Express => {
   const app = express();
   middlewares(app);
   routes(app);
+  swagger(app);
   return app;
 };
--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,7 @@
-import { bodyParser, cors, contentType } from '@/main/middlewares';
+import { bodyParser, cors } from '@/main/middlewares';
 import { Express } from 'express';
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
-  app.use(contentType);
 };
