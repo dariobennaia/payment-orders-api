b3e0c7654e0ec40731dedd41ac1feab35d5fbf36
Dário Bennaia <dario.santos@fortbrasil.com.br>
2022-08-10 23:53:20 -0300

    feat: ensure return not found transfer

--- src/presentation/controllers/find-payment-orders.controller.ts
+++ src/presentation/controllers/find-payment-orders.controller.ts
@@ -1,8 +1,6 @@
 import { FindTransfer } from '@/domain/usecases';
-import {
-  internalServerError,
-  ok,
-} from '@/presentation/helpers';
+import { NotFoundError } from '@/presentation/errors';
+import { internalServerError, ok, notFound } from '@/presentation/helpers';
 import { Controller, HttpResponse } from '@/presentation/protocols';
 
 export class FindPaymentOrdersController implements Controller {
@@ -11,6 +9,7 @@ export class FindPaymentOrdersController implements Controller {
   async handle(id: string): Promise<HttpResponse> {
     try {
       const body = await this.findTransfer.findById(id);
+      if (!body) return notFound(new NotFoundError());
       return ok(body);
     } catch (err) {
       return internalServerError(err);
